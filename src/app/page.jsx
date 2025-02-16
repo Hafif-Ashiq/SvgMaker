5d5e32d10d316fe678260b11bbbe66edfadbea48
Hafif Ashiq <hafifashiq07@gmail.com>
2025-02-17 01:06:21 +0500

    Updated the main screen

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>Main Page</div>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <Link href={"/canvas"} className='px-4 py-2 bg-black rounded-full text-white text-[24px]'>Go to Canvas</Link>
+
+    </div>
   )
 }
 
